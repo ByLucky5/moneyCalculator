1e927857cc9f6a151815b48bb44f64f81c5ab03c
Lucía Motas Guedes <nancimotasguedes@gmail.com>
2025-01-20 23:38:22 +0000

    Delete moneycalculator directory

--- moneycalculator/src/main/java/software/ulpgc/moneycalculator/architecture/view/MoneyDisplay.java
+++
@@ -1,8 +0,0 @@
-package software.ulpgc.moneycalculator.architecture.view;
-
-import software.ulpgc.moneycalculator.architecture.model.Money;
-
-public interface MoneyDisplay {
-    Money get();
-    MoneyDisplay set(Money money);
-}
